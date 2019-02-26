df361167476c5afc99d7999ab00f2f35ac016265
BRIAN TYLER MORA AGUIRRE <brian.moraa@ucuenca.edu.ec>
2019-02-26 15:40:18 -0500

    updates to deploy to heroku

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthenticationService} from "../service/auth.service";
-import {Router} from "@angular/router";
+import {Router, ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -11,9 +11,16 @@ import {Router} from "@angular/router";
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  submitted: boolean = false;
+  submitted = false;
   invalidLogin: boolean = false;
-  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthenticationService) { }
+  returnUrl: string;
+  
+  constructor(
+    private formBuilder: FormBuilder, 
+    private route: ActivatedRoute,
+    private router: Router, 
+    private authService: AuthenticationService
+    ) { }
 
   onSubmit() {
     this.submitted = true;
@@ -32,6 +39,7 @@ export class LoginComponent implements OnInit {
       email: ['', Validators.required],
       password: ['', Validators.required]
     });
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
 }
